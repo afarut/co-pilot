17e6052bdadb789fe0c49334bf0f9efeb7df5162
Dem1dov731 <86346568+Dem1dov731@users.noreply.github.com>
2024-04-28 01:51:30 +0300

    dota2llmcommit
    DOTA2

--- utils.py
+++ utils.py
@@ -2,12 +2,22 @@ import pandas as pd
 
 
 def get_categories(file_path):
-	df = pd.read_csv(file_path)
-	cat = df["Category"]
-	answer = df["answer_class"]
-	result = {}
-	for i in range(len(df)):
-	    if cat[i] not in result:
-	        result[str(cat[i])] = set()
-	    result[str(cat[i])].add(answer[i])
-	return result
+  df = pd.read_csv(file_path)
+  cat = df["Category"]
+  answer = df["answer_class"]
+  result = {}
+  for i in range(len(df)):
+      if cat[i] not in result:
+          result[str(cat[i])] = set()
+      result[str(cat[i])].add(answer[i])
+  return result
+
+
+def get_summars(file_path):
+  df = pd.read_csv(file_path)
+  data = [0] * len(df)
+  answer_class = df["answer_class"]
+  summ = df["summ"]
+  for i in range(len(df)):
+      data[answer_class[i]] = summ[i]
+  return data
